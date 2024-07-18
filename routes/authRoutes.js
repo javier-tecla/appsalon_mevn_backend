6430ab14fe16bef576bb70ea33d00f4ad8b7bf39
javierBorjas <cristman11@gmail.com>
2024-07-18 00:24:45 -0300

    first commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import { register, verifyAccount, login, forgotPassword, verifyPasswordResetToken, updatePassword, user, admin } from '../controllers/authController.js'
+import authMiddleware from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+
+// Rutas de autenticación y registro de usuarios
+router.post('/register', register)
+router.get('/verify/:token', verifyAccount)
+router.post('/login', login)
+router.post('/forgot-password', forgotPassword)
+router.route('/forgot-password/:token')
+    .get(verifyPasswordResetToken)
+    .post(updatePassword)
+
+// Area Privada - Requiere un JWT
+router.get('/user', authMiddleware, user )
+router.get('/admin', authMiddleware, admin )
+
+export default router
